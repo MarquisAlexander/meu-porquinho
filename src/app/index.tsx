1434d63cf00a6d90b8ab38c9d3f65da6de910d7f
marquisalexander <marquis.alexander@outlook.com>
2025-10-09 11:22:31 -0300

    feature: types

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -2,10 +2,16 @@ import { View } from "react-native";
 
 import { HomeHeader } from "@/components/HomeHeader";
 
+const summary = {
+  total: "R$ 2.450,00",
+  input: { label: "Entradas", value: "R$ 5.321,00" },
+  output: { label: "Saídas", value: "-R$ 5.321,00" },
+};
+
 export default function Index() {
   return (
     <View style={{ flex: 1 }}>
-      <HomeHeader data={{ total: "R$ 2.450,00" }} />
+      <HomeHeader data={summary} />
     </View>
   );
 }
--- src/components/HomeHeader/index.tsx
+++ src/components/HomeHeader/index.tsx
@@ -4,10 +4,12 @@ import { colors } from "@/theme/colors";
 
 import { styles } from "./styles";
 import { Separator } from "../Separator";
-import { Summary } from "../Summary";
+import { Summary, SummaryProps } from "../Summary";
 
 export type HomeHeaderProps = {
   total: string;
+  input: SummaryProps;
+  output: SummaryProps;
 };
 
 type Props = {
@@ -29,12 +31,12 @@ export function HomeHeader({ data }: Props) {
 
       <View style={styles.summary}>
         <Summary
-          data={{ label: "Entradas", value: "R$ 5.321,00" }}
+          data={data.input}
           icon={{ name: "arrow-upward", color: colors.green[500] }}
         />
 
         <Summary
-          data={{ label: "Saídas", value: "-R$ 5.321,00" }}
+          data={data.output}
           icon={{ name: "arrow-downward", color: colors.red[400] }}
           isLeft
         />
