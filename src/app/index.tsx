c86049137d12c47f34f74212c5baaa6d00922e08
marquisalexander <marquis.alexander@outlook.com>
2025-10-09 11:37:04 -0300

    feature: add Target component

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,6 +1,7 @@
 import { View } from "react-native";
 
 import { HomeHeader } from "@/components/HomeHeader";
+import { Target } from "@/components/Target";
 
 const summary = {
   total: "R$ 2.450,00",
@@ -8,10 +9,21 @@ const summary = {
   output: { label: "Saídas", value: "-R$ 5.321,00" },
 };
 
+const targets = [
+  {
+    name: "Celular",
+    percentage: "75%",
+    current: "750,00",
+    target: "1.000,00",
+    id: "",
+  },
+];
+
 export default function Index() {
   return (
     <View style={{ flex: 1 }}>
       <HomeHeader data={summary} />
+      <Target data={targets[0]} />
     </View>
   );
 }
