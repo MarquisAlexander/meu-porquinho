7aa7d9343d98a46dac64f65d4735cc507f970ce1
marquisalexander <marquis.alexander@outlook.com>
2025-10-16 14:50:28 -0300

    feature: create pageHeader and input components

--- src/app/target.tsx
+++ src/app/target.tsx
@@ -1,11 +1,19 @@
-import { Button, Text, View } from "react-native";
-import { router } from "expo-router";
+import { View } from "react-native";
+import { PageHeader } from "@/components/PageHeader";
+import { Input } from "@/components/Input";
+import { Button } from "@/components/Button";
 
 export default function Target() {
   return (
-    <View style={{ flex: 1, justifyContent: "center" }}>
-      <Text>Target</Text>
-      <Button title="voltar" onPress={() => router.back()} />
+    <View style={{ flex: 1, padding: 24 }}>
+      <PageHeader
+        title="Meta"
+        subtitle="Economize para alcançar sua meta financeira"
+      />
+      <View style={{ marginTop: 32, gap: 24 }}>
+        <Input label="Nome da meta" placeholder="Ex: viagem para praia" />
+        <Button title="Salvar" />
+      </View>
     </View>
   );
 }
