ae5c3cf68e833e0caf473ac8843925c16137de2a
marquisalexander <marquis.alexander@outlook.com>
2025-10-22 10:30:39 -0300

    feature: add progress component

--- src/app/in-progress/[id].tsx
+++ src/app/in-progress/[id].tsx
@@ -1,15 +1,26 @@
 import { Button, Text, View } from "react-native";
 import { router } from "expo-router";
+import { PageHeader } from "@/components/PageHeader";
+import { Progress } from "@/components/Progress";
+
+const details = {
+  current: "R$ 580,00",
+  target: "R$ 1.790,00",
+  percentage: 25,
+};
 
 export default function Index() {
   return (
-    <View style={{ flex: 1, justifyContent: "center" }}>
-      <Text>in-progress</Text>
-      <Button title="porquinho" onPress={() => router.navigate("/target")} />
-      <Button
-        title="Transação"
-        onPress={() => router.navigate("/transaction/132")}
+    <View style={{ flex: 1, padding: 24, gap: 32 }}>
+      <PageHeader
+        title="Apple watch"
+        rightButton={{
+          icon: "edit",
+          onPress: () => {},
+        }}
       />
+
+      <Progress data={details} />
     </View>
   );
 }
